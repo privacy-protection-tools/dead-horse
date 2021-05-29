638d71e4150a954b2da7dd12eda4788ea9c4030e
gentlyxu <1243610+gentlyxu@users.noreply.github.com>
2021-05-29 16:55:36 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,6 +4,6 @@ source /etc/profile
 
 cd $(cd "$(dirname "$0")";pwd)
 
-curl --connect-timeout 60 -s -o white_domain_list.php https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/master/scripts/lib/white_domain_list.php
+curl --connect-timeout 60 -s -o white_domain_list.php https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/adlist-maker/scripts/lib/white_domain_list.php
 
 php make-white-list.php
