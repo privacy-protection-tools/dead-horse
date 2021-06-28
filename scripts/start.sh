4d2f5b6652ea01acbab579e67bb5f147324c732a
gentlyxu <1243610+gentlyxu@users.noreply.github.com>
2021-06-28 23:38:06 +0800

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,3 +7,6 @@ cd $(cd "$(dirname "$0")";pwd)
 curl --connect-timeout 60 -s -o white_domain_list.php https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/adlist-maker/scripts/lib/white_domain_list.php
 
 php make-white-list.php
+
+cd ..
+sed 's/^\([.0-9A-Za-z\-]\+\)$/address \/\1\/-/g' anti-ad-white-list.txt >anti-ad-white-for-smartdns.txt
