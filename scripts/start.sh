37cf261c2e5ff7de2e3314e193db1f5b8d099b03
gentlyxu <1243610+gentlyxu@users.noreply.github.com>
2022-07-03 12:47:15 +0800

    update.supported clash payload

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,3 +10,7 @@ php make-white-list.php
 
 cd ..
 sed 's/^\([.0-9A-Za-z\-]\+\)$/address \/\1\/-/g' anti-ad-white-list.txt >anti-ad-white-for-smartdns.txt
+
+echo '#RULE-SET,AntiAd,DIRECT' >anti-ad-white-for-clash.yaml
+echo 'payload:' >>anti-ad-white-for-clash.yaml
+sed "s/^\([.0-9A-Za-z\-]\+\)$/  - '+.\1'/g" anti-ad-white-list.txt >>anti-ad-white-for-clash.yaml
