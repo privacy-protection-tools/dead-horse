22fab4a27208e07d066657b9f55172185d416cfd
GitHub Action <action@github.com>
2023-06-03 02:29:44 +0000

    Auto renew list.

--- scripts/white_domain_list.php
+++ scripts/white_domain_list.php
@@ -399,6 +399,9 @@ return [
     "www.mairuan.net" => 0, #772
     "ldmnq.com" => 0, #787
     "heytapimage.com" => 0, #779, #780
+    "fexclick.baidu.com" => 0, #790
+    "jango.com" => 0, #797
+    "huluxia.com" => 0, #798
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
     "puds.ucweb.com" => 0, // uc小游戏
